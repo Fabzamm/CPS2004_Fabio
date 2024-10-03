0248838ff9df0f1fd5a0524a49d34f988e442baf
Fabio Zammit <fabio.zammit.23@um.edu.mt>
2024-10-03 11:58:45 +0200

    Test Commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/HP/OneDrive/School/Uni/2nd Year/Semester 1/CPS2004 - Object Oriented Programming/Tutorials/CPS2004_Fabio
+# Build directory: /mnt/c/Users/HP/OneDrive/School/Uni/2nd Year/Semester 1/CPS2004 - Object Oriented Programming/Tutorials/CPS2004_Fabio/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- main.cpp
+++ main.cpp
@@ -1,5 +1,77 @@
 #include <iostream>
 
+// Wherever you want to use the namespace, it will be included
+using namespace std;
+
+class MyClass { // syntax follows the c syntax for structs
+
+    // attributes functions methods and fields
+private:
+    string field1;
+    int field2;
+
+public:
+    // default constructor
+    MyClass() : field1("Hello"), field2(2) { // initializer list
+        cout << "Default constructor called" <<endl; 
+    }
+
+    // parameterized constructor
+    MyClass(string field1arg, int field2arg) : field1(field1arg), field2(field2arg) {
+        cout << "Parameterized constructor called" <<endl; 
+    }
+
+    ~MyClass() {
+        cout << "Destructor called" <<endl; 
+    }
+
+    // Setters
+    void setField1(const string& field1) {
+        // in a setter make the argument const to not be updated
+        // passed by reference
+        this->field1 = field1; // this ptr
+    }
+    void setField2(const int field2) {
+        // in a setter make the argument const to not be updated
+        // passed by value since it is a primitive object
+        this->field2 = field2; // this ptr
+    }
+
+    // Getters
+    const string& getField1(){
+        return field1;
+    }
+    const int getField2(){
+        return field2;
+    }
+
+};
+
 int main(int, char**){
-    std::cout << "Hello, from Test!\n";
+    // std:: is called a namespace
+    // allow us to create functions, classes 
+    std::cout << "Hello, from helloWorld!\n"; //printf()
+
+    MyClass myObj1, myObj2("Hello2", 5);
+    myObj1.setField1("Hello1");
+    myObj1.setField2(2);
+
+    // Ex to display the fields of obj1 and obj2 to cout
+    // Display fields of myObj1
+    std::cout << "myObj1.field1: " << myObj1.getField1() << std::endl;
+    std::cout << "myObj1.field2: " << myObj1.getField2() << std::endl;
+
+    // Display fields of myObj2
+    std::cout << "myObj2.field1: " << myObj2.getField1() << std::endl;
+    std::cout << "myObj2.field2: " << myObj2.getField2() << std::endl;
+
+    // Heap allocation example (creating an object on the heap)
+    MyClass* myObjHeap = new MyClass("HeapObject", 10); // Allocated on the heap
+    std::cout << "myObjHeap->field1: " << myObjHeap->getField1() << std::endl;
+    std::cout << "myObjHeap->field2: " << myObjHeap->getField2() << std::endl;
+
+    // Deallocate the heap object
+    delete myObjHeap; // Calls the destructor for myObjHeap
+
+    // Command Shift P to do the cmake run etc
 }
